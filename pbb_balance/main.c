d2363673d0d6b1f717e37b1b48fb6fb92ac676b8
maxicot <st140678@student.spbu.ru>
2025-10-16 18:00:43 +0300

    Implement parenthesis, bracket, brace balance check

---
+++ pbb_balance/main.c
@@ -0,0 +1,71 @@
+#include "../linked_list/linked_list.h"
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+
+// Return the opening opposite to one of the closing characters (0 if invalid)
+char opener(char ch)
+{
+    switch (ch) {
+        case ')':
+            return '(';
+        case ']':
+            return '[';
+        case '}':
+            return '{';
+        default:
+            return 0;
+    }
+}
+
+// Check whether parens, brackets and braces in the given null-terminated string are balanced
+bool isBalanced(const char* str)
+{
+    LinkedList stack = lnew();
+    size_t len = strlen(str);
+    size_t index = 0;
+
+    while (index < len) {
+        char ch = str[index];
+
+        switch (ch) {
+            case '(':
+            case '[':
+            case '{':
+                lpush(&stack, ch);
+                break;
+            case ')':
+            case ']':
+            case '}':
+                PopResult last = lpop(&stack);
+
+                if (!(last.valid && last.value == opener(ch))) {
+                    return false;
+                }
+
+                break;
+        }
+
+        index++;
+    }
+
+    return llen(&stack) == 0;
+}
+
+int main(void)
+{
+    const char* testCases[6] = {
+        "",
+        "(",
+        ")",
+        "()",
+        "k((aa{a[b{ba[c(c)c]}]c}sajd)l)w",
+        "(([{]}))",
+    };
+
+    for (size_t i = 0; i < 6; i++) {
+        printf("%d: '%s'\n", isBalanced(testCases[i]), testCases[i]);
+    }
+
+    return 0;
+}
